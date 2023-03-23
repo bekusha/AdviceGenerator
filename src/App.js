16b2b435010342a71b6c717cb6b3ef145d2dd6d8
Jorjikia Beka <jorjikiabeka@Jorjikias-MacBook-Air.local>
2023-03-23 18:50:19 +0400

    create folder

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import { useState } from "react";
+import shape from "./Shape.png";
+import logo from "./Group.png";
+import line from "./Rectangle.png";
 function App() {
+let array = ['asfasf','adfafdaf']
+
+  const arr = [
+    "Take time to know yourself. ",
+    "A narrow focus brings big results.",
+    "Show up fully. Don't dwell on the past, and don't daydream about the future, but concentrate on showing up fully in the present moment.",
+    "Don't make assumptions. If you don't know the situation fully, you can't offer an informed opinion.",
+  ];
+  const [randomAdvice, setRandomAdvice] = useState("Advice");
+  
+
+  const randomArrItem = () => {
+    const value = Math.floor(Math.random() * arr.length);
+    setRandomAdvice(arr[value]);
+    console.log(value)
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="head">Get </div>
+      <div className="adviceContainer">
+      <p>{randomAdvice}</p>
+      </div>
+      <div className="lineLogo">
+        <div className="leftLine">
+          <img src={line} />
+        </div>
+        <img className="logo" src={logo} />
+        <div className="rightLine">
+          <img src={line} />
+        </div>
+      </div>
+      <div className="button" onClick={randomArrItem}>
+        <img src={shape} />
+      </div>
     </div>
   );
 }
